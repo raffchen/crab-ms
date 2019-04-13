3ffeecc9eec17683a7bd387327d331690df82df3
johnyu763 <39107518+johnyu763@users.noreply.github.com>
2019-04-13 15:25:12 -0700

    Delete crab.py

--- src/crab.py
+++
@@ -1,9 +0,0 @@
-class Crab(Animal):
-    def __init__(self, game):
-        self._type = "crab"
-        self.game = game
-
-    def move(self, c_dict):
-        if(0 <= self.pos[self.direct[0]]+self.direct[1] \
-           < self.game._bounds):
-            c_dict[self._type]
