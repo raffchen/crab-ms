c9ba34470274aebcf14bc2b7816184d1870f6f95
MagicTurtle <raffertychen@gmail.com>
2019-04-13 00:03:54 -0700

    added quitting and resizing

--- src/game.py
+++ src/game.py
@@ -1,5 +1,5 @@
 from random import randrange
 class Game:
     def __init__(self):
-        self._running = True
+        self.running = True
 
--- src/game_runner.py
+++ src/game_runner.py
@@ -4,23 +4,36 @@ from game import Game
 
 class GameView:
     def __init__(self, game_state):
-        self.size_tuple = (400, 600)
+        self.size_tuple = (700, 450)
         self.game = game_state
         self.counter = 0
 
     def run(self):
         """initializes, executes, and quits the pygame"""
         pygame.init()
-        self._resize_screen((400, 600))
-        self.clock = pygame.time.Clock()
-        while self.counter < 100:
+
+        self._resize_screen(self.size_tuple)
+        clock = pygame.time.Clock()
+
+        while self.game.running:
+            clock.tick(60)
+
+            self._handle_events()
             self._display_board()
-            self.counter += 1
+
+        pygame.quit()
     
     def _display_board(self):
         """displays the board when it changes"""
         self.screen.fill(pygame.Color(255, 255, 255))
         pygame.display.flip()
+
+    def _handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.game.running = False
+            elif event.type == pygame.VIDEORESIZE:
+                self._resize_screen(event.size)
 
     def _resize_screen(self, size: (int,int)) -> None:
         """changes the size of the game screen"""
