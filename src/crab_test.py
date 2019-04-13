d077c36e3dfc380b084de198962635049abf8e63
MagicTurtle <raffertychen@gmail.com>
2019-04-12 23:29:47 -0700

    fixed formatting to comply with pep8

--- src/crab_test.py
+++ src/crab_test.py
@@ -1,8 +1,10 @@
 import unittest
 
+
 class CrabTest(unittest.TestCase):
     def setUp(self):
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
--- src/game_runner.py
+++ src/game_runner.py
@@ -4,27 +4,28 @@ from game import Game
 
 class GameView:
     def __init__(self, game_state):
-        self.size_tuple = (400,600)
+        self.size_tuple = (400, 600)
         self.game = game_state
         self.counter = 0
 
     def run(self):
-        '''initializes, executes, and quits the pygame'''
+        """initializes, executes, and quits the pygame"""
         pygame.init()
-        self._resize_screen((400,600))
+        self._resize_screen((400, 600))
         self.clock = pygame.time.Clock()
-        while(self.counter < 100):
+        while self.counter < 100:
             self._display_board()
-            self.counter+=1
+            self.counter += 1
     
     def _display_board(self):
-        '''displays the board when it changes'''
-        self.screen.fill(pygame.Color(255,255,255))
+        """displays the board when it changes"""
+        self.screen.fill(pygame.Color(255, 255, 255))
         pygame.display.flip()
 
     def _resize_screen(self, size: (int,int)) -> None:
-        '''changes the size of the game screen'''
+        """changes the size of the game screen"""
         self.screen = pygame.display.set_mode(size, pygame.RESIZABLE) 
+
 
 if __name__ == '__main__':
     game = Game()
