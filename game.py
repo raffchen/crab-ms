93b82da8c6b336de4d88370cb45da5ff6c8b94aa
johnyu763 <39107518+johnyu763@users.noreply.github.com>
2019-04-12 23:14:23 -0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,5 @@
+from random import randrange
+class Game:
+    def __init__(self):
+        self._running = True
+
--- game_runner.py
+++ game_runner.py
@@ -1,26 +1,33 @@
 import pygame
+from game import Game
+
 
 class GameView:
-    def __init__(self):
+    def __init__(self, game_state):
         self.size_tuple = (400,600)
+        self.game = game_state
+        self.counter = 0
 
     def run(self):
         '''initializes, executes, and quits the pygame'''
         pygame.init()
         self._resize_screen((400,600))
         self.clock = pygame.time.Clock()
-
+        while(self.counter < 100):
+            self._display_board()
+            self.counter+=1
+    
     def _display_board(self):
         '''displays the board when it changes'''
         self.screen.fill(pygame.Color(255,255,255))
-        self._build_board()
         pygame.display.flip()
-    
+
     def _resize_screen(self, size: (int,int)) -> None:
         '''changes the size of the game screen'''
         self.screen = pygame.display.set_mode(size, pygame.RESIZABLE) 
 
 if __name__ == '__main__':
-    view = GameView()
+    game = Game()
+    view = GameView(game)
     view.run()
 
