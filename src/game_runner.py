6ced2fe43ceb84fadd2becc0a1585e79d884747e
johnyu763 <39107518+johnyu763@users.noreply.github.com>
2019-04-13 15:38:04 -0700

    Update game_runner.py

--- src/game_runner.py
+++ src/game_runner.py
@@ -1,6 +1,6 @@
 import pygame
 from pathlib import Path
-from random import random, choice, shuffle
+from random import random, choice, choices
 import map_generator
 
 
@@ -29,6 +29,7 @@ class Crab(Player):
             "vision": {"status": False, "timer": 0},
             "weakness": {"status": False, "timer": 0}
         }
+        self.speed = 4
 
     def update_location(self, move):
         self._location = (self._location[0]+move[0], self._location[1]+move[1])
@@ -42,6 +43,7 @@ class Crab(Player):
             if self._index >= len(self._standing_still_animation_frames):
                 self._index = 0
             self.img = self._standing_still_animation_frames[self._index]
+
 
 class Pebble():
     
@@ -93,29 +95,31 @@ class GameView:
         self.player = Crab(str(Path("./data/images/crab.png")), (72, 44), (300, 200))
         # TEST
         self.player.symptoms['loss-of-balance']['status'] = True
+        self.player.symptoms['fatigue']['status'] = True
         # END TEST
         self.pebbles = []
         self.end_screen = Player(str(Path("./data/images/endgame.png")), (700, 450))
         self.gulls = [Seagull(str(Path("./data/images/seagull.png")), (72, 44), self.background),
                       Seagull(str(Path("./data/images/seagull.png")), (72, 44), self.background),
                       Seagull(str(Path("./data/images/seagull.png")), (72, 44), self.background)]
-        self.h_bars = [Player(str(Path("./data/images/health0.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health1.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health2.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health3.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health4.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health5.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health6.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health7.png")), (216, 134), (10,-30)),\
-                       Player(str(Path("./data/images/health8.png")), (216, 134), (10,-30))]
+        self.h_bars = [Player(str(Path("./data/images/health0.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health1.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health2.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health3.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health4.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health5.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health6.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health7.png")), (216, 134), (10, -30)),
+                       Player(str(Path("./data/images/health8.png")), (216, 134), (10, -30))]
 
-        self.moves = {"up": (0, 4), "left": (4, 0), "down": (0, -4), "right": (-4, 0)}
+        self.moves = {"up": (0, self.player.speed), "left": (self.player.speed, 0),
+                      "down": (0, -self.player.speed), "right": (-self.player.speed, 0)}
         pygame.mixer.init()
         pygame.mixer.music.set_volume(0.7)
         pygame.mixer.music.load("./data/music/Intro.mp3")
         pygame.mixer.music.play() 
-        
-             
+
+
     def run(self):
         """initializes, executes, and quits the pygame"""
         pygame.init()
@@ -135,7 +139,7 @@ class GameView:
 
     def _display_board(self):
         """displays the board when it changes"""
-        self.screen.fill(pygame.Color(255, 255, 255))
+        self.screen.fill(pygame.Color(0, 0, 0))
         #self.screen.blit(self.background.img, self.background.rect)
         map_generator.loadLevel(self.screen, 'level1.txt')
         #for gull in self.gulls:
@@ -152,13 +156,12 @@ class GameView:
             self.screen.blit(self.end_screen.img, self.end_screen.rect)
         self.screen.blit(self.h_bars[int(self.player.health/10)].img, self.h_bars[int(self.player.health/10)].rect)
         pygame.display.flip()
-
     
 
     def _handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.game.running = False
+                self.running = False
             elif event.type == pygame.MOUSEBUTTONUP:
                 self.shoot(pygame.mouse.get_pos())
         keys = pygame.key.get_pressed()
@@ -173,11 +176,58 @@ class GameView:
                 self._move("right")
         else:
             if keys[pygame.K_r]:
-                self.__init__(Game())
+                self.__init__()
                 pygame.mixer.music.load("./data/music/Crab.mp3")
                 pygame.mixer.music.play() 
                 map_generator.default_x_coord = map_generator.DEFAULT_STARTING_X_COORD
                 map_generator.default_y_coord = map_generator.DEFAULT_STARTING_Y_COORD
+
+    def _move(self, key):
+        for symptom, flag in self.player.symptoms.items():
+            if symptom == 'loss-of-balance' and flag["status"]:
+                if flag["timer"] == 0:
+                    self._moves = self.moves.copy()
+                    self.moves = dict(zip(sorted(self.moves.keys(), key=lambda x: random()),
+                                      sorted(self.moves.values(), key=lambda x: random())))
+                    self.player.symptoms[symptom]["timer"] += 1
+                elif flag["timer"] == 150:
+                    self.moves = self._moves
+                    del self._moves
+                    self.player.symptoms[symptom]["status"] = False
+                    self.player.symptoms[symptom]["timer"] = 0
+                else:
+                    self.player.symptoms[symptom]["timer"] += 1
+
+            elif symptom == 'fatigue' and flag["status"]:
+                if flag["timer"] == 0:
+                    self.player.speed = choices([0, 1, 2, 3], [5, 10, 10, 25])[0]/4
+                    self.moves = {k: tuple(map(lambda x: int(x * self.player.speed), v)) for k, v in self.moves.items()}
+                    self.player.symptoms[symptom]["timer"] += 1
+                elif flag["timer"] == 150:
+                    self.player.speed = 4
+                    self.player.symptoms[symptom]["status"] = False
+                    self.player.symptoms[symptom]["timer"] = 0
+                    try:
+                        self.moves = {k: tuple(map(lambda x: int(x * self.player.speed/x), v))
+                                      for k, v in self.moves.items()}
+                    except ZeroDivisionError:
+                        self.moves = {"up": (0, self.player.speed), "left": (self.player.speed, 0),
+                                      "down": (0, -self.player.speed), "right": (-self.player.speed, 0)}
+                else:
+                    self.player.symptoms[symptom]["timer"] += 1
+                
+        if ((key == "up" and not self.player.rect.top <= self.background.rect.top)
+                or (key == "left" and not self.player.rect.left <= self.background.rect.left)
+                or (key == "down" and not self.player.rect.bottom >= self.background.rect.bottom)
+                or (key == "right" and not self.player.rect.right >= self.background.rect.right)):
+            self.background.rect = self.background.rect.move(*self.moves[key])
+            if self.moves[key][0] != 0:
+                map_generator.default_x_coord += self.moves[key][0]
+            elif self.moves[key][1] != 0:
+                map_generator.default_y_coord += self.moves[key][1]
+
+        if random() > 0.9:
+            self.player.health -= 1        
     
     def shoot(self, mouse_click):
         vector_direction = [0,0]
@@ -193,29 +243,6 @@ class GameView:
         if vector_direction != (0,0):
             self.pebbles.append(Pebble(vector_direction, self.player.get_location()))
 
-    def _move(self, key):
-        moves = {"up": (0, 4), "left": (4, 0), "down": (0, -4), "right": (-4, 0)}
-        
-        #if random() < 0.20:
-         #    key = choice(["up", "left", "right", "down"])
-                
-        if ((key == "up" and not self.player.rect.top <= self.background.rect.top)
-            or (key == "left" and not self.player.rect.left <= self.background.rect.left)
-            or (key == "down" and not self.player.rect.bottom >= self.background.rect.bottom)
-            or (key == "right" and not self.player.rect.right >= self.background.rect.right)):
-            self.background.rect = self.background.rect.move(*moves[key])
-            if moves[key][0] != 0:
-                map_generator.default_x_coord += moves[key][0]
-            elif moves[key][1] != 1:
-                map_generator.default_y_coord += moves[key][1]
-        
-        for pebble in self.pebbles:
-            if moves[key][0] != 0:
-                pebble.location = (pebble.location[0] + moves[key][0], pebble.location[1])
-            elif moves[key][1] != 1:
-                pebble.location = (pebble.location[0], pebble.location[1] + moves[key][1])
-        '''else:
-             self._character_move(key)'''
 
 if __name__ == '__main__':
     view = GameView()
