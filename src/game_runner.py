afe65cefcc9fa751d32523c3e04c669ca0b210dc
manksharma <2mayanksharma2@gmail.com>
2019-04-13 14:54:15 -0700

    added pebble throwing, underwater tiles, fixed boundary issue

--- src/game_runner.py
+++ src/game_runner.py
@@ -40,8 +40,20 @@ class Crab(Player):
             if self._index >= len(self._standing_still_animation_frames):
                 self._index = 0
             self.img = self._standing_still_animation_frames[self._index]
-
             
+        
+class Pebble():
+    
+    img = pygame.transform.scale(pygame.image.load("./data/images/pebble.png"), (20, 20))
+    speed = 5
+    
+    def __init__(self, vector_direction, location):
+        self.vector_direction = vector_direction
+        self.location = location
+        
+    def update(self):
+        self.location = (self.location[0] + self.vector_direction[0]*Pebble.speed, self.location[1] + self.vector_direction[1]*Pebble.speed)
+
         
 
 class Seagull(Player):
@@ -76,10 +88,11 @@ class GameView:
     def __init__(self, game_state):
         self.game = game_state
         self.screen = pygame.display.set_mode((700, 450))
-        self.background = Player(str(Path("./data/images/beach.jpg")), (map_generator.ABSOLUTE_BORDER_SIZE*map_generator.IMAGE_WIDTH, map_generator.ABSOLUTE_BORDER_SIZE*map_generator.IMAGE_HEIGHT))
+        self.background = Player(str(Path("./data/images/beach.jpg")), (int(0.5*map_generator.ABSOLUTE_BORDER_SIZE*map_generator.IMAGE_WIDTH), int(0.5*map_generator.ABSOLUTE_BORDER_SIZE*map_generator.IMAGE_HEIGHT)))
         self.player = Crab(str(Path("./data/images/crab_images/Crab Standing Animation/crab_standing_still0.png")), (60, 60), (300, 200))
         self.end_screen = Player(str(Path("./data/images/endgame.png")), (700, 450))
-
+        
+        self.pebbles = []
         #self.gulls = [Seagull(str(Path("./data/images/seagull.png")), (72, 44), self.background),
         #              Seagull(str(Path("./data/images/seagull.png")), (72, 44), self.background),
         #              Seagull(str(Path("./data/images/seagull.png")), (72, 44), self.background)]
@@ -93,6 +106,7 @@ class GameView:
                        Player(str(Path("./data/images/health6.png")), (216, 134), (237,-10)),\
                        Player(str(Path("./data/images/health7.png")), (216, 134), (237,-10)),\
                        Player(str(Path("./data/images/health8.png")), (216, 134), (237,-10))]
+        
 
     def run(self):
         """initializes, executes, and quits the pygame"""
@@ -118,6 +132,10 @@ class GameView:
         map_generator.loadLevel(self.screen, 'level1.txt')
         #for gull in self.gulls:
         #    self.screen.blit(gull.img, gull.rect)
+        
+        for pebble in self.pebbles:
+            self.screen.blit(Pebble.img, pebble.location)
+            pebble.update()
         if self.player.health > 0:
             self.player.update()
             self.screen.blit(self.player.img, self.player.rect)
@@ -131,6 +149,8 @@ class GameView:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.game.running = False
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.shoot(pygame.mouse.get_pos())
         keys = pygame.key.get_pressed()
         if self.player.health > 0:
             if keys[pygame.K_w] or keys[pygame.K_UP]:
@@ -144,25 +164,47 @@ class GameView:
         else:
             if keys[pygame.K_r]:
                 self.__init__(Game())
+                map_generator.default_x_coord = map_generator.DEFAULT_STARTING_X_COORD
+                map_generator.default_y_coord = map_generator.DEFAULT_STARTING_Y_COORD
+    
+    def shoot(self, mouse_click):
+        vector_direction = [0,0]
+        if self.player.get_location()[0] - mouse_click[0] < 0:
+            vector_direction[0] = 1
+        elif self.player.get_location()[0] - mouse_click[0] > 0:
+            vector_direction[0] = -1
+        if self.player.get_location()[1] - mouse_click[1] < 0:
+            vector_direction[1] = 1
+        elif self.player.get_location()[1] - mouse_click[1] > 0:
+            vector_direction[1] = -1
+        vector_direction = tuple(vector_direction)
+        if vector_direction != (0,0):
+            self.pebbles.append(Pebble(vector_direction, self.player.get_location()))
 
     def _move(self, key):
         moves = {"up": (0, 4), "left": (4, 0), "down": (0, -4), "right": (-4, 0)}
         
         #if random() < 0.20:
          #    key = choice(["up", "left", "right", "down"])
-        
-      
                 
-        if ((key == "up" and not self.player.rect.top <= self.background.rect.top+200)
-            or (key == "left" and not self.player.rect.left <= self.background.rect.left+300)
-            or (key == "down" and not self.player.rect.bottom >= self.background.rect.bottom-250)
-            or (key == "right" and not self.player.rect.right >= self.background.rect.right-350)):
+        if ((key == "up" and not self.player.rect.top <= self.background.rect.top)
+            or (key == "left" and not self.player.rect.left <= self.background.rect.left)
+            or (key == "down" and not self.player.rect.bottom >= self.background.rect.bottom)
+            or (key == "right" and not self.player.rect.right >= self.background.rect.right)):
             self.background.rect = self.background.rect.move(*moves[key])
+            if moves[key][0] != 0:
+                map_generator.default_x_coord += moves[key][0]
+            elif moves[key][1] != 1:
+                map_generator.default_y_coord += moves[key][1]
+        
+        for pebble in self.pebbles:
+            if moves[key][0] != 0:
+                pebble.location = (pebble.location[0] + moves[key][0], pebble.location[1])
+            elif moves[key][1] != 1:
+                pebble.location = (pebble.location[0], pebble.location[1] + moves[key][1])
         '''else:
              self._character_move(key)'''
 
-        if random() > 0.9:
-            self.player.health -= 1
 
     def _character_move(self, key):
          moves = {"up": (0, 4), "left": (4, 0), "down": (0, -4), "right": (-4, 0)}
@@ -180,6 +222,7 @@ class GameView:
                  or (key == "right" and self.player.rect.right < self.background.rect.right+72)):
              self.player.rect = self.player.rect.move(*moves[key])
              self.player.update_location(moves[key])
+             
 
 if __name__ == '__main__':
     game = Game()
