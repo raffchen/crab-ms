10305b364324aecdb0b03181809af4e244d61848
johnyu763 <39107518+johnyu763@users.noreply.github.com>
2019-04-13 18:48:10 -0700

    Add files via upload

--- src/littlefish.py
+++ src/littlefish.py
@@ -1,16 +1,36 @@
+import pygame
+from pathlib import Path
+from player import Player
 
 class LittleFish(Player):
-    def __init__(self, image: str, size: tuple, location: tuple = (0, 0), crab):
-        Player.__init__(image,size,location)
+    img = pygame.transform.scale(pygame.image.load(str(Path("./data/images/littlefish.png"))), (35, 35))
+    def __init__(self, crab, location: tuple = (0, 0)):
+        self.image = Jellyfish.img
         self.health = 10
         self.location = location
-        
+        self.crab = crab
+        self.speed = 5
+        self.lst = lst
 
     def _contains(self):
-        if(((crab.get_location()[0]-self.location[0])**2)+\
-           (crab.get_location()[0]-self.location[0])**2 < 17):
+        if(((crab.get_location()[0]-self.get_location()[0])**2)+\
+           (crab.get_location()[0]-self.get_location()[0])**2 < 17):
             return True
         return False
 
-    
+    def update(self):
+        if(self._contains()):
+            if(self.crab.health<80):
+                self.crab.health+=10
+            else:
+                self.crab.health = 80
+            return
+        elif(pygame.time.get_ticks() % 2 == 0):
+            if(10<self.location[0]<690):
+                self.location = (self.location[0]+speed,self.location[1])
+        return self
+
+    def get_location(self):
+        return self.location()
+                
         
